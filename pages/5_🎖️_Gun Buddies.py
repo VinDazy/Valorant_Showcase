49ca9ba725285f431e5672474134c0bf78d4822b
VinDazy <hectorvalinci@gmail.com>
2023-08-27 16:49:27 +0100

    availibility emojis

--- pages/5_🎖️_Gun Buddies.py
+++ pages/5_🎖️_Gun Buddies.py
@@ -22,32 +22,32 @@ for buddy in json_data['data']:
         buddy_name = buddy['displayName']
         buddy_image = buddy['displayIcon']
         if buddy['isHiddenIfNotOwned']==True:
-            availability=False
+            availability='  🚫'
         else:
-            availability=True
+            availability='  ✔️'
         if buddy_count <= 61:
             col1.image(buddy_image, caption=f"{buddy_name} \n",width=120)
-            col1.write(f"Availability : {availability}")
+            col1.write(f"Available : {availability}")
             col1.markdown("---")
         elif buddy_count in range(62, 123):
             col2.image(buddy_image, caption=f"{buddy_name} \n",width=120)
-            col2.write(f"Availability : {availability}")
+            col2.write(f"Available : {availability}")
             col2.markdown("---")
         elif buddy_count in range(123, 185):
             col3.image(buddy_image, caption=f"{buddy_name} \n",width=120)
-            col3.write(f"Availability : {availability}")
+            col3.write(f"Available : {availability}")
             col3.markdown("---")
         elif buddy_count in range(185, 247):
             col4.image(buddy_image, caption=f"{buddy_name} \n",width=120)
-            col4.write(f"Availability : {availability}")
+            col4.write(f"Available : {availability}")
             col4.markdown("---")
         elif buddy_count in range(247, 309):
             col5.image(buddy_image, caption=f"{buddy_name} \n",width=120)
-            col5.write(f"Availability : {availability}")
+            col5.write(f"Available : {availability}")
             col5.markdown("---")
         elif buddy_count in range(309, 367):  # Corrected range here
             col6.image(buddy_image, caption=f"{buddy_name}\n",width=120)
-            col6.write(f"Availability : {availability}")
+            col6.write(f"Available : {availability}")
             col6.markdown("---")
         else:
             # Handle any remaining buddies or cases not covered in the ranges
