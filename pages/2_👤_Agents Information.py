2f509c62fd9ebebca6f6c18d980fff8a9ed0613e
VinDazy <hectorvalinci@gmail.com>
2023-09-03 23:11:08 +0100

    added ability description list

--- pages/2_👤_Agents Information.py
+++ pages/2_👤_Agents Information.py
@@ -41,6 +41,8 @@ if search:
                 biography=agent['description']
                 ability_images=[]
                 ability_names=[]
+                ability_description=[]
+
                 agent_role=agent['role']['displayName']
                 role_description=agent['role']['description']
                 role_img=agent['role']['displayIcon']
@@ -52,6 +54,7 @@ if search:
                 for ability in agent['abilities']:
                     ability_names.append(ability['displayName'])
                     ability_images.append(ability['displayIcon'])
+                    ability_description.append(ability['description'])
                 with pic:
                     pic.subheader(f"{Agent_name} Portrait")
                     pic.image(image,caption=f"{Agent_name}",use_column_width=True)
@@ -62,10 +65,14 @@ if search:
                     description.markdown("---")
                     description.subheader(f"{Agent_name} Abilities")
                     ab1,ab2,ab3,ult=description.columns(4)
-                    ab1.image(image=ability_images[0],caption=ability_names[0])
-                    ab2.image(image=ability_images[1],caption=ability_names[1])
-                    ab3.image(image=ability_images[2],caption=ability_names[2])
-                    ult.image(image=ability_images[3],caption=ability_names[3])
+                    with ab1:
+                        ab1.image(image=ability_images[0],caption=ability_names[0])
+                    with ab2:
+                        ab2.image(image=ability_images[1],caption=ability_names[1])
+                    with ab3:
+                        ab3.image(image=ability_images[2],caption=ability_names[2])
+                    with ult :
+                        ult.image(image=ability_images[3],caption=ability_names[3])
                     description.markdown("---")
                     if agent_tags is not None:
                         description.subheader(f"{Agent_name} Tags")
